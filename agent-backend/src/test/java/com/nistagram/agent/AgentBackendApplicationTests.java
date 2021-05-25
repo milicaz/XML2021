e0a08d86ebdc0701642efcb8ba797358c15c3492
milicaz <zm025555@gmail.com>
2021-05-25 16:38:39 +0200

    Agent backend

---
+++ agent-backend/src/test/java/com/nistagram/agent/AgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nistagram.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
