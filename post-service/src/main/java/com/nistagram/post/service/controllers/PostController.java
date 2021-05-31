7cdf706608e99dffcfe47213c3df7c62266cd619
milicaz <zm025555@gmail.com>
2021-05-31 12:35:42 +0200

    Izmena

--- post-service/src/main/java/com/nistagram/post/service/controllers/PostController.java
+++ post-service/src/main/java/com/nistagram/post/service/controllers/PostController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("post")
+@RequestMapping("/post")
 public class PostController {
 	
 	@GetMapping("/status/check")
