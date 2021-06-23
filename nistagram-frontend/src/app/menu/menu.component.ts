ae51cc81bdcd90670b462db7e32c40e6eb33b61c
Tamara Puskarov <tamara.puskarov@gmail.com>
2021-06-23 16:43:02 +0200

    Jos izmena oko dodavanja posta

--- nistagram-frontend/src/app/menu/menu.component.ts
+++ nistagram-frontend/src/app/menu/menu.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
-import { LogoutComponent } from '../logout/logout.component';
-import { LoginDataService } from '../service/login-data.service';
+import { HttpClient, HttpEventType, HttpResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { LoginComponent } from "../login/login.component";
+import { LogoutComponent } from "../logout/logout.component";
+import { LoginDataService } from "../service/login-data.service";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"],
 })
 export class MenuComponent implements OnInit {
-
   // isUserLoggedIn : boolean = false
 
-  constructor(private loginService : LoginDataService) {
-   }
+  selectedFile: File;
+  progress = 0;
+  message = "";
+  imageName: any;
+  retrievedImage: any;
+  retrieveResponse: any;
+  base64Data: any;
+  
+  constructor(
+    private http: HttpClient,private loginService: LoginDataService
+  ) {}
 
   ngOnInit() {
     // console.log("User je: " + this.isUserLoggedIn)
@@ -21,4 +31,22 @@ export class MenuComponent implements OnInit {
     //console.log("User je: " + this.isUserLoggedIn)
   }
 
+  selectFile(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  upload() {
+    this.progress = 0;
+
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    this.http.post('http://localhost:8400/media/upload', uploadImageData, {observe: 'response'}).subscribe((response)=>{
+      if(response.status === 200){
+        this.message = 'Image uploaded successfully.';
+      } else {
+        this.message = 'Image could not be uploaded.';
+      }
+    });
+  }
 }
