88aaa8b7ed526088d9bc33bf3964e01d5298a176
milicaz <zm025555@gmail.com>
2021-07-09 19:44:23 +0200

    Zapracivanje drugih profila

--- nistagram-frontend/src/app/follow/follow.component.ts
+++ nistagram-frontend/src/app/follow/follow.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileModel, ProfileService } from '../service/profile.service';
 
 @Component({
   selector: 'app-follow',
@@ -9,10 +10,37 @@ import { Router } from '@angular/router';
 })
 export class FollowComponent implements OnInit {
 
+  profiles : Array<ProfileModel>
+
+  profile : any
+
+  image : any
+  u : any
+  log : any
+  username : any
+
   constructor(
+    private profileService : ProfileService
   ) { }
 
   ngOnInit() {
+    this.profiles = new Array<ProfileModel>()
+    this.username = sessionStorage.getItem('logUser')
+    this.profileService.executeGetProfiles().subscribe(
+      data => {
+        this.image = 'data:image/jpeg;base64,'
+          for(this.u in data) {
+            if(this.username !== data[this.u].username){
+              this.profile = data[this.u]
+              this.profiles.push(this.profile)
+            }
+            else{
+              console.log("Usernmae je isti")
+            }
+          }
+      }
+    )
   }
+
 
 }
